3531fdaa2543d33e18d8ed2d2b341537d2947e70
Zach Stenberg <Zach.Stenberg@symetra.com>
2019-08-19 14:22:00 -0700

    Added factoextra to prepMachine.R

--- prepMachine.R
+++ prepMachine.R
@@ -1,7 +1,8 @@
 list.of.packages <- c( 
   "tidyverse", 
   "readxl", 
-  "e1071")
+  "e1071",
+  "factoextra")
 new.packages <- list.of.packages[!(list.of.packages %in% 
                                      installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages)
